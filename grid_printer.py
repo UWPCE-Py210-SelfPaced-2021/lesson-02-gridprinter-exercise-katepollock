bd5a1097c02d5f240c096f035be8e4c6ed4cf09a
katepollock <katepollock@outlook.com>
2022-02-05 15:01:42 -0800

    completed grid printer assignment 2

--- grid_printer.py
+++ grid_printer.py
@@ -4,21 +4,50 @@ Update below functions with code to print different grids based on the input par
 NOTE: do not print anything besides the grid boxes in your functions.
 """
 
-# Hello
+# Kate Pollock
+# Python 310
+# Assignment 2: Grid Printer
 
 # PART 1
+# print a 2x2 grid using a function
+
 def print_grid1():
-    pass
+    size = 4
+    rows_cols = 2
+    for i in range(rows_cols):
+        print(rows_cols * ('+ ' + ('- ' * size)) + '+')
+        for j in range(size):
+            print(rows_cols * ('| ' + ('  ' * size)) + '|')
+    print(rows_cols * ('+ ' + ('- ' * size)) + '+')
 
 
 # PART 2
+# print a 2x2 grid using a function that has one parameter 'size'
+# 'size' is assumed to be the number of characters between the borders
+# in the case that 'size' is an odd number, use '//' to round down
+
 def print_grid2(size):
-    pass
+    n = size // 2
+    for i in range(2):
+        print(2 * ('+ ' + (n * '- ')) + '+')
+        for j in range(n):
+            print(2 * ('| ' + (n * '  ')) + '|')
+    print(2 * ('+ ' + (n * '- ')) + '+')
+
 
 
 # PART 3
+# print a grid using a function that has 2 parameters
+# box_size represents the number of rows or columns which are equivalent
+# cell_size represents the number of spaces between the borders of the rows or columns
+
 def print_grid3(box_size, cell_size):
-    pass
+    for i in range(box_size):
+        print(box_size * ('+ ' + (cell_size * '- ')) + '+')
+        for j in range(cell_size):
+            print(box_size * ('| ' + (cell_size * '  ')) + '|')
+    print(box_size * ('+ ' + (cell_size * '- ')) + '+')
+
 
 # executes your grid functions below
 if __name__ == "__main__":
